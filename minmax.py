8f137c40dd4df65e0dfcdd599e975eef3ef26c5e
Jeremy Gillespie <jeremy@gillespiegames.com>
2021-01-14 10:24:08 -0800

    refactor

--- minmax.py
+++ minmax.py
@@ -9,26 +9,42 @@ class state:
         self.min_moves = 1
         self.chance_dist = [1.0]
         self.game_over = True
-        self.value = 0
-        self.strategy = (np.array([1.0]), np.array([1.0]))
+        self._updated = False
+        self._value = 0
+        self._game = nash.Game(np.array([[1]], dtype=float))
 
     def successor(self, max_m=0, min_m=0, chance_m=0):
         return state()
 
-    def update(self):
-        if not self.game_over:
+    def _update(self):
+        if not self.game_over and not self._updated:
+            self._updated = True
             self._calc_strat()
+
+    def _get_strats(self):
+        self._update()
+        return self._game.support_enumeration()
+
+    strategies = property(_get_strats)
+
+    def _get_value(self):
+        self._update()
+        return self._value
+
+    def _set_value(self, val):
+        self._value = val
+
+    value = property(_get_value, _set_value)
 
     def _calc_strat(self):
         payoffs = np.empty((self.max_moves, self.min_moves), dtype=float)
 
         for max_m in range(self.max_moves):
             for min_m in range(self.min_moves):
-                payoffs[min_m, max_m] = self._avg_outcome(max_m, min_m)
+                payoffs[max_m, min_m] = self._avg_outcome(max_m, min_m)
 
-        game = nash.Game(payoffs)
-        self.strategy = next(game.support_enumeration())
-        self.value = game[self.strategy][0]
+        self._game = nash.Game(payoffs)
+        self._value = self._game[next(self.strategies)][0]
 
     def _avg_outcome(self, max_m, min_m):
         result = 0
@@ -65,12 +81,25 @@ class rps(state):
         return result
 
 
-r1 = rps()
-r1.update()
-print(r1.value)
-print(r1.strategy)
+class test(state):
+    def __init__(self):
+        state.__init__(self)
+        self.max_moves = 3
+        self.game_over = False
 
-r2 = r1.successor(rps.rock, rps.paper)
-r2.update()
+    def successor(self, max_m=0, min_m=0, chance_m=0):
+        result = test()
+        result.value = 1.0
+        result.game_over = True
+        return result
+
+
+r1 = test()
+print(r1.value)
+for s in r1.strategies:
+    print(s)
+
+r2 = r1.successor(0)
 print(r2.value)
-print(r2.strategy)
+for s in r2.strategies:
+    print(s)
